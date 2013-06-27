c1e9626fc8597d84281caff1cf7d9dc19d0740f9
Aaron Contorer test2 <aaron+fpguy2@fpcomplete.com>
2013-06-27 02:42:47 +0000

    gray background

--- src/Main.hs
+++ src/Main.hs
@@ -29,7 +29,7 @@ mkYesod "Gravity" [parseRoutes|
 |]
 
 boxColor   = "#000"::String
-bodyColor  = "#338"::String  -- it's blue  now.
+bodyColor  = "#aaa"::String  -- it's gray now.
 boxSizeX    = 600::Int
 boxSizeY    = 600::Int
 framesPerS = 16::Int
